bfc9ec1e9cea68920b2689c280754cf1d3242fac
hjortmotiv0n <142341384+hjortmotiv0n@users.noreply.github.com>
2023-09-16 14:10:59 +0200

    adding CVE 20

---
+++ CVE20Ex01.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CVE20Ex01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter your age: ");
+        String ageInput = scanner.nextLine();
+
+        // This part of the code doesn't validate the input properly, which can
+        // lead to improper behavior of the program
+        try {
+            int age = Integer.parseInt(ageInput);
+            if (age < 0) {
+                System.out.println("Age cannot be negative.");
+            } else {
+                System.out.println("Thank you! Your age is recorded as " + age);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a valid number.");
+        }
+    }
+}
